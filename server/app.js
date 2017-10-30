ee22ac60c17a4d3f3b1678f177332493366380f5
dimasgardenia <dimas.gardenia@gmail.com>
2017-10-30 19:33:35 +0700

    add mongodb atlas

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 
-mongoose.connect('mongodb://localhost/ecommerce')
+mongoose.connect("mongodb://dimasgardenia:dima$1990@cluster0-shard-00-00-vtgnv.mongodb.net:27017,cluster0-shard-00-01-vtgnv.mongodb.net:27017,cluster0-shard-00-02-vtgnv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
